c76842e74d8111f9b64acd5feec5574f50d429fc
Visethboti <visethboti@gmail.com>
2022-02-27 12:52:26 -0600

    Working

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('container').oninput = function() {
+  var nRed = document.getElementById('rInput').value;
+  var nBlue = document.getElementById('bInput').value;
+  var nGreen = document.getElementById('gInput').value;
+
+  document.getElementById('newRed').innerHTML = nRed;
+  document.getElementById('newBlue').innerHTML = nBlue;
+  document.getElementById('newGreen').innerHTML = nGreen;
+  document.getElementById('colorBox').style.background = 'rgb('+nRed+','+nBlue+','+nGreen+')';
+  //'rgb('+nRed+','+nBlue+','+nGreen+')'
+}
